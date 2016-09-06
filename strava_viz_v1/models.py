f68c56720f81993157daa998db4daa4e4ce4014c
Andrew Norelli <andrewnorelli@mac.com>
2016-09-05 21:12:37 -0400

    adding models and views data.

--- strava_viz_v1/models.py
+++ strava_viz_v1/models.py
@@ -3,3 +3,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Workout(models.Model):
+	user = models.CharField(max_length=15)
+	location = models.CharField(max_length=55)
+	date = models.DateTimeField()
+	miles = models.FloatField()
+	workout_type = models.ForeignKey('WorkoutType', on_delete=models.CASCADE)
+	vertical_ascent = models.IntField()
+
+class WorkoutType(models.Model):
+	RUN = 'RUN'
+	BIKE = 'BIKE'
+	SWIM = 'SWIM'
--- strava_viz_v1/views.py
+++ strava_viz_v1/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django import forms
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+from .models import Workout
+
+
+class WorkoutForm(forms.Form):
+	"""Form Object"""
+	submitter = forms.CharField('Your Name')
+	location = forms.CharField('Where was your workout?')
+	date = forms.DateTimeField('When was your workout?')
+	miles = forms.FloatField('How many miles did you go?')
+	workout_type = forms.ForeignKey('Which type of workout?')
+	vertical_ascent = forms.IntField('How many feet did you climb?')
+	
